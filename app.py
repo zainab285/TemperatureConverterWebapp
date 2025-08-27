0c9122c7cb50de1ebdd53c20b2f976ad163d0b11
zainab285 <zainabpathan17567@gmail.com>
2025-08-27 13:48:53 -0700

    Add startup print for debugging

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+print("❇️  Starting Flask app on Render...")
 
 def convert_temperature(temp, from_scale, to_scale):
     # Convert input temperature to Celsius first
