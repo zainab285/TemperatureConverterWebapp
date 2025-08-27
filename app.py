f9cad817d46d0b4459761730a1758bbc8f06be3d
zainab285 <zainabpathan17567@gmail.com>
2025-08-27 15:09:40 -0700

    Enable debug mode in Flask

--- app.py
+++ app.py
@@ -51,8 +51,7 @@ def temperature_converter():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
+
 
 
