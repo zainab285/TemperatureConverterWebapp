5eaeb2118829948e77f6aed3d8a2d4f943d0ac43
zainab285 <zainabpathan17567@gmail.com>
2025-08-27 13:41:20 -0700

    Fix app.run placement for Render

--- app.py
+++ app.py
@@ -50,4 +50,8 @@ def temperature_converter():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
