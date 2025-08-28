9df10863b5b016e6af9b116f451fa885df5d1e07
zainab285 <zainabpathan17567@gmail.com>
2025-08-27 22:17:36 -0700

    Fix app to listen on 0.0.0.0 and use Render PORT

--- app.py
+++ app.py
@@ -51,7 +51,10 @@ def temperature_converter():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 
 
